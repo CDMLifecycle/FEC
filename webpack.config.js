d4de713f0941ec04bb269f13bd1c701dd3adb8c4
Daniel Kosykh <dankosykh@gmail.com>
2021-02-24 13:27:24 -0800

    set up webpack 5

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './client/src/index.jsx',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'client/dist')
+  },
+  devServer: {
+    contentBase: './client/dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        include: path.join(__dirname, 'client/src'),
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+        }
+       }
+      }
+    ]
+  }
+};
