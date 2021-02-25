fdb2b317d2b651d3677ee2b19d6543b992064c71
Liem Nguyen <shxinx@gmail.com>
2021-02-25 15:13:33 -0800

    Product get Request in client called

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,24 @@
 import React from 'react';
+import axios from 'axios';
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      productArr:  [],
+    }
+  }
+
+  componentDidMount() {
+    axios.get('/products')
+    .then((res) => {
+      console.log(res.data);
+    })
+    .catch((error) => {
+      console.log(error);
+    })
+  }
+
   render() {
     return (
     <form>
