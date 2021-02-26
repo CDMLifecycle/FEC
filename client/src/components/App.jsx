b715603c442d32c14b4a63d400536489be1f0947
Liem Nguyen <shxinx@gmail.com>
2021-02-25 17:36:14 -0800

    Made the navigation page

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
 import axios from 'axios';
+import {AppBar, Toolbar, Typography, IconButton} from '@material-ui/core';
+
 class App extends React.Component {
   constructor() {
     super();
 
     this.state = {
       productArr:  [],
+      input: '',
+      searched: '',
     }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState({searched: this.state.input, input: ''});
+  }
+  handleChange(event) {
+    this.setState({input: event.target.value});
   }
 
   componentDidMount() {
@@ -22,8 +36,20 @@ class App extends React.Component {
 
   render() {
     return (
-    <form>
-      <div> App renders properly </div>
+    <form onSubmit={this.handleSubmit}>
+      <AppBar position="static" elevation={0} >
+        <Toolbar>
+          <Typography type="title" color="inherit">
+            CLMD ENTERPRISE
+          </Typography>
+          <input value={this.state.input} onChange={this.handleChange}></input>
+          <IconButton className='material-icons' onClick={this.handleSubmit}>
+            <img src='https://image.winudf.com/v2/image1/c2Fuc3Vuc2VuMy5pbWFnZXNlYXJjaGVyX3NjcmVlbl83XzE1NDI5MDQ2MTNfMDUx/screen-7.jpg?fakeurl=1&type=.jpg' width="25" height="25"/>
+          </IconButton>
+          <div>
+          </div>
+        </Toolbar>
+      </AppBar>
     </form>
     );
   }
