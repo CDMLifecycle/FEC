6a4e50e6dbac6cf76a0916212cd2c13260c316a6
Daniel Kosykh <dankosykh@gmail.com>
2021-02-27 15:56:44 -0800

    added R&R component to App

---
+++ client/src/components/ratingsAndReviews/RatingsAndReviews.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class RatingsAndReviews extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {};
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>Ratings and Reviews</h1>
+      </div>
+    )
+  }
+}
+
+export default RatingsAndReviews;
