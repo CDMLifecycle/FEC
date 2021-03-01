d863cee05d580a8ba4ee90436d84160445e71835
Liem Nguyen <shxinx@gmail.com>
2021-03-01 01:00:52 -0800

    Added a landing page where it's just the search

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,7 +4,9 @@ import ProductDetail from './Product_rendering/Product_Detail.jsx';
 import NavBar from './NavBar.jsx';
 import RelatedItems from './relatedProducts/RelatedItems.jsx';
 import RatingsAndReviews from './ratingsAndReviews/RatingsAndReviews.jsx';
+import Landing from './landing.jsx';
 var stringSimilarity = require("string-similarity");
+
 
 class App extends React.Component {
   constructor() {
@@ -15,9 +17,12 @@ class App extends React.Component {
       searchedQuery: '',
       productID: '',
       searchedArr: [],
+      paths: '/'
     }
     this.handleSubmitForm = this.handleSubmitForm.bind(this);
     this.stringComparion = this.stringComparion.bind(this);
+    this.switchStatement = this.switchStatement.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
     // this.matchSearches = this.matchSearches.bind(this);
   }
 
@@ -27,7 +32,7 @@ class App extends React.Component {
       var string1 = this.state.productArr[i].name;
       var string2 = this.state.searchedQuery;
       var similarity = stringSimilarity.compareTwoStrings(string1, string2);
-      if (similarity > 0.3) {
+      if (similarity > 0.15) {
         arr.push(this.state.productArr[i]);
       }
     }
@@ -50,19 +55,36 @@ class App extends React.Component {
   }
   handleSubmit(event) {
     event.preventDefault();
+    console.log(this.state.paths);
+    if(this.state.paths !== '/final') {
+      this.setState({paths: '/final'});
+    }
+  }
+
+  switchStatement() {
+    switch(this.state.paths) {
+      case "/":
+        return <NavBar handleSubmitForm={this.handleSubmitForm} handleSubmit={this.handleSubmit}/>
+      case "/final":
+        return (
+          <div>
+            <NavBar handleSubmitForm={this.handleSubmitForm}/>
+            <ProductDetail productID={this.state.productID} searched={this.state.searchedQuery} searchedArr={this.state.searchedArr}/>
+            <RelatedItems />
+            <RatingsAndReviews productID={this.state.productID}/>
+          </div>
+        )
+      }
   }
 
   render() {
     return (
     <form onSubmit={this.handleSubmit}>
-      <NavBar handleSubmitForm={this.handleSubmitForm}/>
-      {/* addCode after this */}
-      <ProductDetail productID={this.state.productID} searched={this.state.searchedQuery} searchedArr={this.state.searchedArr}/>
-      <RelatedItems />
-      <RatingsAndReviews productID={this.state.productID}/>
-    </form>
-    );
+      {this.switchStatement()}
+    </form>)
   }
 }
 
 export default App;
+
+
