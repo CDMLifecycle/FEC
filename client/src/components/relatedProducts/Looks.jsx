f69e068d4d0f54a34425770c9b2656e12a937455
Michael Smith <mijamessmith@gmail.com>
2021-03-01 12:28:17 -0800

    added skeleton components for Looks

---
+++ client/src/components/relatedProducts/Looks.jsx
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+import ProductCard from './ProductCard.jsx';
+import AddToLooksCard from './AddToLooksCard.jsx';
+
+var Looks (props) => {
+  return (
+    <div className='Looks'>
+      <AddToLooksCard />
+    </div>
+  )
+}
+
+export default Looks;
