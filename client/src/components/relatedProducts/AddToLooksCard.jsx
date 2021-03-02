b64d077c79139b853fa8d3dd1a3842fdcd6b64af
Michael Smith <mijamessmith@gmail.com>
2021-03-02 10:28:51 -0800

    added styling to carousel components

--- client/src/components/relatedProducts/AddToLooksCard.jsx
+++ client/src/components/relatedProducts/AddToLooksCard.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 var AddToLooksCard = function(props) {
   return (
     <div className='AddToLooksCard'>
-      <p>Add Current Item to Your Looks</p>
+      <div className='AddToLooksCard-Icon'>+</div>
+      <p className='AddToLooksCard-p'>Add Current Item to Your Looks</p>
     </div>
   )
 }
