5586c2c8e7f817ae64e937184a772398151bbf3c
Daniel Kosykh <dankosykh@gmail.com>
2021-03-03 11:11:17 -0800

    Fixed up calculator js. More metadata added

--- server/controller/calculator.js
+++ server/controller/calculator.js
@@ -1,45 +1,44 @@
 const calc  = {
 
   averageRatingToQtr: (ratings) => {
-    let total = 0
-    let numOfRatings = 0;
-    for (let key in ratings) {
-      total+= parseInt(key) * parseInt(ratings[key]);
-      numOfRatings += parseInt(ratings[key]);
-    }
-    if (numOfRatings > 0) {
-      var average = total / numOfRatings
-      return Math.floor(average * 4) / 4;
-    } else {
-      return null;
-    }
+    let average = calc.avgRating(ratings)
+    return average ? Math.round(average * 4) / 4 : null
   },
 
   sumObjValues: (property) => {
     let sum = 0;
+    let flag = false;
     for (let key in property) {
       sum += parseInt(property[key])
+      flag = true;
     }
-    return sum;
+    return flag ? sum : null;
   },
 
   avgRating: (property) => {
     let totalRatings = calc.sumObjValues(property);
     let keyValueSum = 0;
+    let flag = false;
     for (let rating in property) {
       keyValueSum += parseInt(rating) * parseInt(property[rating])
+      flag = true;
     }
-    return keyValueSum / totalRatings;
+    return flag ? Number((keyValueSum / totalRatings).toFixed(2)) : null;
+  },
+
+  percent: (favorable, total) => {
+    return ((favorable / total).toFixed(2)) * 100;
   },
 
   calcMeta: (metaDataObject) => {
     let totals = {
-      totalRatings: calc.sumObjValues(metaDataObject.ratings).toFixed(2),
-      avgRatings: calc.avgRating(metaDataObject.ratings).toFixed(2),
+      totalRatings: calc.sumObjValues(metaDataObject.ratings),
+      avgRating: calc.avgRating(metaDataObject.ratings),
+      avgRatingsToQtr: calc.averageRatingToQtr(metaDataObject.ratings),
       sumRecommendations: calc.sumObjValues(metaDataObject.recommended),
-      percentRecommend: metaDataObject.recommended.true / calc.sumObjValues(metaDataObject.recommended).toFixed(2)
+      percentRecommend: calc.percent(metaDataObject.recommended.true, calc.sumObjValues(metaDataObject.recommended))
     }
-    return { totals } ;
+    return { totals };
   }
 
 }
