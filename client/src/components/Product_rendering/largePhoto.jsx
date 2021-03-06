623a0e17fce98155c1109480b80a50e818ae9316
Liem Nguyen <shxinx@gmail.com>
2021-03-05 23:25:20 -0800

    Implmented sliding functionality on pictures

--- client/src/components/Product_rendering/largePhoto.jsx
+++ client/src/components/Product_rendering/largePhoto.jsx
@@ -1,7 +1,17 @@
 import Magnifier from 'react-magnifier';
+import { AnimateOnChange } from 'react-animation';
+import { animations } from 'react-animation'
 
 var largePhoto = (props) => {
   return (props.photo ?
-  <Magnifier onClick={props.fullscreen} className='largeImage' id='lg' src={props.photo} width='auto' height='600'></Magnifier> : <div></div>)
+    <AnimateOnChange
+      animationIn="slidein 500ms ease-out forwards"
+      animationOut="slideout 500ms ease-in forwards"
+      durationOut={700}
+      durationIn={0}
+    >
+      <Magnifier onClick={props.fullscreen} className='largeImage' id='lg' src={props.photo} width='auto' height='600'></Magnifier>
+    </AnimateOnChange>
+: <div></div>)
 }
 export default largePhoto;
