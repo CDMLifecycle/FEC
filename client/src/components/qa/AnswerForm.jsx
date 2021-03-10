b0b73c61b3c243d4f07250418dd6132fe22b6170
foodforcode <tsai.connie@yahoo.com>
2021-03-09 21:13:27 -0800

    Add styling details and change AnswerForm to stateful component

--- client/src/components/qa/AnswerForm.jsx
+++ client/src/components/qa/AnswerForm.jsx
@@ -1,43 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const AnswerForm = props => {
-  const divStyle = {
-    display: props.displayModal ? 'block' : 'none'
-  }
-  function closeModal(e) {
-    e.stopPropagation()
-    props.closeModal(e)
+class AnswerForm extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      question: '',
+      nickname: '',
+      email: ''
+    }
+    this.closeModal = this.closeModal.bind(this);
+    this.submitAForm = this.submitAForm.bind(this);
+
   }
 
-  function submitAForm(e) {
+  closeModal(e) {
+    e.stopPropagation()
+    this.props.addAnswer(e)
+  }
+
+  submitAForm(e) {
     e.preventDefault();
+    this.closeModal(e);
     console.log('submit answer form');
   }
 
-  let AForm = (
-    <div className="qaModal" onClick={closeModal}
-    style={divStyle}>
-      <div className="Modal-Content" onClick={e => e.stopPropagation()}>
-        <span className="close" onClick={closeModal}>Close</span>
-        <form onSubmit={submitAForm}>
-          <h3>Submit your answer</h3>
-          <h5 className="qa_subtitle" >{props.productName}: {props.q_body}</h5>
-          <label className="qa_label" name="Your Question">Your Answer</label><br />
-          <textarea className="qInput qa-modal-form" type="text" maxLength="1000" placeholder="ENTER YOUR QUESTION..." autoComplete="off" required /><br />
-          <label className="qa_label" name="nickname">Nickname</label><br />
-          <input className="qInput" placeholder="EXAMPLE: jack543!" autoComplete="off" required /><br />
-          <label className="qa_label" name="email">Email</label><br />
-          <input className="qInput" placeholder="EXAMPLE: JACKSON@EMAIL.COM" maxLength="60" autoComplete="off" required /><br />
-          <button className="submitAnswer">SUBMIT ANSWER</button>
-        </form>
+  render () {
+    const divStyle = {
+      display: this.props.displayModal ? 'block' : 'none'
+    }
+    return (
+      <div>
+        {this.props.displayModal ?
+        (
+          <div className="qaModal" onClick={this.closeModal}
+          style={divStyle}>
+            <div className="Modal-Content" onClick={e => e.stopPropagation()}>
+              <span className="close" onClick={this.closeModal}>X</span>
+              <form onSubmit={this.submitAForm}>
+                <h3>Submit your answer</h3>
+                <h5 className="qa_subtitle" >{this.props.productName}: {this.props.q_body}</h5>
+                <label className="qa_label" name="Your Question">Your Answer</label><br />
+                <textarea className="qInput qa-modal-form" type="text" maxLength="1000" placeholder="ENTER YOUR QUESTION..." autoComplete="off" required /><br />
+                <label className="qa_label" name="nickname">Nickname</label><br />
+                <input className="qInput" placeholder="EXAMPLE: jack543!" autoComplete="off" required /><br />
+                <label className="qa_label" name="email">Email</label><br />
+                <input className="qInput" placeholder="EXAMPLE: JACKSON@EMAIL.COM" maxLength="60" autoComplete="off" required /><br />
+                <button className="submitAnswer">SUBMIT ANSWER</button>
+              </form>
+            </div>
+          </div>
+        )
+        : (null)}
       </div>
-    </div>
-  )
-
-  return (
-    props.displayModal ? (AForm) : (null)
-  );
+    );
+  }
 }
 
 export default AnswerForm;
