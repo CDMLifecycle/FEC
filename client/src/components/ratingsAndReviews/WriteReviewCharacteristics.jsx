493d90083ee5e4f480ed5d65109f85916dd49673
Daniel Kosykh <dankosykh@gmail.com>
2021-03-10 23:52:30 -0800

    add modal styling

--- client/src/components/ratingsAndReviews/WriteReviewCharacteristics.jsx
+++ client/src/components/ratingsAndReviews/WriteReviewCharacteristics.jsx
@@ -41,57 +41,62 @@ class WriteReviewCharacteristics extends React.Component {
     console.log(this.props)
     let name = this.props.characteristic.toLowerCase();
     return (
-          <div className='per-characteristic-box'>
-            {this.props.characteristic}
-            <label onChange={this.props.handleChange}>
-              <label >
-                {this.assignSpecifics(this.props.characteristic)[0]}
-                <input
-                  type='radio'
-                  value='1'
-                  name={name}
-                  className={this.assignSpecifics(this.props.characteristic, 'className')[0]}
-                  required
-                />
-              </label>
-              <label>
-                {this.assignSpecifics(this.props.characteristic)[1]}
-                <input
-                  type='radio'
-                  value='2'
-                  name={name}
-                  className={this.assignSpecifics(this.props.characteristic, 'className')[1]}
-                />
-              </label>
-              <label>
-                {this.assignSpecifics(this.props.characteristic)[2]}
-                <input
-                  type='radio'
-                  value='3'
-                  name={name}
-                  className={this.assignSpecifics(this.props.characteristic, 'className')[2]}
-                />
-              </label>
-              <label>
-                {this.assignSpecifics(this.props.characteristic)[3]}
-                <input
-                  type='radio'
-                  value='4'
-                  name={name}
-                  className={this.assignSpecifics(this.props.characteristic, 'className')[3]}
-                />
-              </label>
-              <label>
-                {this.assignSpecifics(this.props.characteristic)[4]}
-                <input
-                  type='radio'
-                  value='5'
-                  name={name}
-                  className={this.assignSpecifics(this.props.characteristic, 'className')[4]}
-                />
-              </label>
-            </label>
-          </div>
+      <div className='per-characteristic-box'>
+        {this.props.characteristic}
+        <label onChange={this.props.handleChange} className='wr-char-radio-box'>
+          <label className='wr-char-input-box' for={this.props.characteristic + '1'}>
+            {this.assignSpecifics(this.props.characteristic)[0]}
+            <input
+              type='radio'
+              value='1'
+              name={name}
+              className={this.assignSpecifics(this.props.characteristic, 'className')[0]}
+              id={this.props.characteristic + '1'}
+              required
+            />
+          </label>
+          <label className='wr-char-input-box' for={this.props.characteristic + '2'}>
+            {this.assignSpecifics(this.props.characteristic)[1]}
+            <input
+              type='radio'
+              value='2'
+              name={name}
+              className={this.assignSpecifics(this.props.characteristic, 'className')[1]}
+              id={this.props.characteristic + '2'}
+            />
+          </label>
+          <label className='wr-char-input-box' for={this.props.characteristic + '3'}>
+            {this.assignSpecifics(this.props.characteristic)[2]}
+            <input
+              type='radio'
+              value='3'
+              name={name}
+              className={this.assignSpecifics(this.props.characteristic, 'className')[2]}
+              id={this.props.characteristic + '3'}
+            />
+          </label>
+          <label className='wr-char-input-box' for={this.props.characteristic + '4'}>
+            {this.assignSpecifics(this.props.characteristic)[3]}
+            <input
+              type='radio'
+              value='4'
+              name={name}
+              className={this.assignSpecifics(this.props.characteristic, 'className')[3]}
+              id={this.props.characteristic + '4'}
+            />
+          </label>
+          <label className='wr-char-input-box' for={this.props.characteristic + '5'}>
+            {this.assignSpecifics(this.props.characteristic)[4]}
+            <input
+              type='radio'
+              value='5'
+              name={name}
+              className={this.assignSpecifics(this.props.characteristic, 'className')[4]}
+              id={this.props.characteristic + '5'}
+            />
+          </label>
+        </label>
+      </div>
     )
   }
 }
