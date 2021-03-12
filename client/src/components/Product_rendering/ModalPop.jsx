428b68ca019dba6765f427e49fce3ad865c4adb3
Liem Nguyen <shxinx@gmail.com>
2021-03-11 21:44:55 -0800

    Finished with css and tests

---
+++ client/src/components/Product_rendering/ModalPop.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+class ModalPop extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      name: '',
+      email: '',
+      input: '',
+      value: '',
+    }
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.backgroundColor = this.backgroundColor.bind(this);
+  }
+
+  onChange(event) {
+    event.preventDefault();
+    this.setState({[event.target.name] : event.target.value});
+  }
+  onSubmit(event) {
+    event.preventDefault();
+    this.props.submit(this.state.value, this.state.input);
+    this.props.clicked();
+  }
+  backgroundColor(event) {
+    if(event.target.tagName === 'DIV') {
+      this.props.clicked();
+    }
+  }
+
+  render() {
+    return (
+      <div className='modalBackground' id='modal' onClick={this.backgroundColor}>
+        <form onSubmit={this.onSubmit} className='ProductModal'>
+          <button className='xBox'>X</button>
+          Name: <input name='input' className='ModalContents' onChange={this.onChange} value={this.state.input}></input>
+          Email: <input className='ModalContents' name='value' onChange={this.onChange} value={this.state.value}></input>
+          <button onClick={this.onSubmit}>OK</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default ModalPop;
--- spec/client/components/productDetail/largePhoto.test.jsx
+++ spec/client/components/productDetail/largePhoto.test.jsx
@@ -4,17 +4,18 @@ import LargePhoto from '../../../../client/src/components/Product_rendering/larg
 import Magnifier from 'react-magnifier';
 
 // /Users/liemnguyen/hfso133/Capstone/Fec2/client/src/components/Product_rendering
+
 describe('LargePhotos', () => {
-  it("renders largePhoto from Product_Detail without crashing", () => {
+  const props = {
+    photo: 'https://images.unsplash.com/photo-1501088430049-71c79fa3283e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80'
+  }
+  it("renders left transition", () => {
     shallow(<LargePhoto photo={'https://images.unsplash.com/photo-1501088430049-71c79fa3283e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80'} fullscreen={false}  direction={'left'}/>)
   });
   it("renders right transition", () => {
-    const props = {
-      photo: 'https://images.unsplash.com/photo-1501088430049-71c79fa3283e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80'
-    }
-
-    var wrapper = shallow(<LargePhoto photo={props.photo} fullscreen={false}  direction={'left'}/>);
-
-    expect(wrapper.find('div.largeImageL').length).toBe(1);
+    shallow(<LargePhoto photo={props.photo} fullscreen={false}  direction={'right'}/>);
+  });
+  it("renders no transition", () => {
+    shallow(<LargePhoto photo={props.photo} fullscreen={false}  direction={''}/>);
   });
 });
